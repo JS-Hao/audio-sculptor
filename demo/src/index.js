d4e377231f138b16f8a74c48ea3c0dd339a7f887
zhuzhihao <zhuzhihao@cvte.com>
2020-05-27 11:38:38 +0800

    update:demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -7,44 +7,21 @@ async function main() {
   const audioSculptor = new AudioSculptor();
 
   // 启动audioSculptor
-  const p = await audioSculptor.open({
+  await audioSculptor.open({
     workerPath: '/static/ffmpeg-worker-mp4.js',
     mediaType: 'mp3',
   });
 
   // 创建一个音频
   const audio = document.querySelector('audio');
-
   const blob = await audioSculptor.toBlob(audio);
 
-  // const arrayBuffer = await blobToArrayBuffer(blob);
-  // const audioContext = new AudioContext();
-  // const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-  // const duration = parseFloat(audioBuffer.duration.toFixed(3)) * 1000;
-  // audioContext.close();
-
-  // 裁剪第5至50秒
-  const beginTime = Date.now();
-  // const clippedBlob = await audioSculptor.splice(blob, 60 * 60, 60 * 70);
-  const clippedBlob = await audioSculptor.convert(blob, 'webm', 60 * 60 * 1000);
-  const endTime = Date.now();
+  // 裁剪第5至10秒
+  const clippedBlob = await audioSculptor.clip(blob, 5, 10);
   const clippedAudio = await audioSculptor.toAudio(clippedBlob);
 
   const span = document.querySelector('.span');
   span.innerText = '处理后的音频: ';
   clippedAudio.controls = true;
   document.body.appendChild(clippedAudio);
-  const h1 = document.createElement('h1');
-  h1.innerText = endTime - beginTime;
-  document.body.appendChild(h1);
-}
-
-function blobToArrayBuffer(blob) {
-  return new Promise(resolve => {
-    const fileReader = new FileReader();
-    fileReader.onload = function() {
-      resolve(fileReader.result);
-    };
-    fileReader.readAsArrayBuffer(blob);
-  });
 }
